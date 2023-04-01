34dd8315f5d222e65b4e2fb279dc1b310b2bab26
kk-min <76023265+kk-min@users.noreply.github.com>
2023-04-01 12:58:05 +0800

    Add tests for custom type

--- pq_test.go
+++ pq_test.go
@@ -1,60 +1,116 @@
 package pq
 
 import (
-	"reflect"
 	"testing"
 )
 
-func TestPQCreation(t *testing.T) {
+type CustomItem struct {
+	Key string
+	Val int
+}
+
+func TestPQCreationInt(t *testing.T) {
 	q := CreatePQ[int](func(a, b int) bool { return a < b })
 	if q == nil {
-		t.Error("PQ creation failed")
+		t.Error("PQ Creation failed")
 	}
 }
 
 func TestPQPutInt(t *testing.T) {
 	q := CreatePQ[int](func(a, b int) bool { return a < b })
 	if q == nil {
-		t.Error("PQ creation failed")
+		t.Error("PQ Creation failed")
 	}
 
 	q.Put(5)
 	q.Put(3)
+	q.Put(1)
+	q.Put(1)
 
-	temp := []int{}
-	for _, v := range q.Items {
-		temp = append(temp, *v)
+	if len(q.Items) != 4 {
+		t.Error("PQ PutInt failed")
 	}
 
-	if !reflect.DeepEqual(temp, []int{3, 5}) {
-		t.Error("PQ Put failed: ", q.Items)
-	}
 }
 
 func TestPQGetInt(t *testing.T) {
 	q := CreatePQ[int](func(a, b int) bool { return a < b })
 	if q == nil {
-		t.Error("PQ creation failed")
+		t.Error("PQ Creation failed")
 	}
 
 	q.Put(5)
 	q.Put(3)
 
 	if q.Get() != 3 {
-		t.Error("PQ Get failed")
+		t.Error("PQ GetInt failed")
 	}
 }
 
-func TestPQGetPeek(t *testing.T) {
+func TestPQPeekInt(t *testing.T) {
 	q := CreatePQ[int](func(a, b int) bool { return a < b })
 	if q == nil {
-		t.Error("PQ creation failed")
+		t.Error("PQ Creation failed")
 	}
 
 	q.Put(5)
 	q.Put(3)
 
 	if q.Peek() != 3 {
-		t.Error("PQ Get failed")
+		t.Error("PQ Peek failed")
+	}
+}
+
+func TestPQCreationCustom(t *testing.T) {
+	q := CreatePQ[CustomItem](func(a, b CustomItem) bool { return a.Val < b.Val })
+	if q == nil {
+		t.Error("PQ Creation failed")
+	}
+}
+
+func TestPQPutCustom(t *testing.T) {
+	q := CreatePQ[CustomItem](func(a, b CustomItem) bool { return a.Val < b.Val })
+	if q == nil {
+		t.Error("PQ Creation failed")
+	}
+
+	q.Put(CustomItem{"a", 5})
+	q.Put(CustomItem{"b", 3})
+	q.Put(CustomItem{"c", 1})
+
+	if len(q.Items) != 3 {
+		t.Error("PQ PutCustom failed")
+	}
+}
+
+func TestPQGetCustom(t *testing.T) {
+	q := CreatePQ[CustomItem](func(a, b CustomItem) bool { return a.Val < b.Val })
+	if q == nil {
+		t.Error("PQ Creation failed")
+	}
+
+	q.Put(CustomItem{"a", 5})
+	q.Put(CustomItem{"b", 3})
+	q.Put(CustomItem{"c", 1})
+
+	head := q.Get()
+	if head.Key != "c" || head.Val != 1 {
+		t.Error("PQ GetCustom failed")
+	}
+}
+
+func TestPQPeekCustom(t *testing.T) {
+	q := CreatePQ[CustomItem](func(a, b CustomItem) bool { return a.Val < b.Val })
+	if q == nil {
+		t.Error("PQ Creation failed")
+	}
+
+	q.Put(CustomItem{"a", 5})
+	q.Put(CustomItem{"b", 3})
+	q.Put(CustomItem{"c", 1})
+
+	head := q.Peek()
+	if head.Key != "c" || head.Val != 1 {
+		t.Error("PQ PeekCustom failed")
 	}
 }
